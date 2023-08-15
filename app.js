d9acd4cf86e00122dec0026f8f3ea89cbe238dad
@moin12345 <moinchoudary2003@gmail.com>
2023-08-15 17:15:00 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput)
+
+
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "mjmjmjmjmjmj " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
