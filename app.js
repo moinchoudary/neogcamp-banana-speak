e7859e444976888ef7dc0d2788bedd8384353f27
@moin12345 <moinchoudary2003@gmail.com>
2023-08-18 15:08:48 +0530

    done

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
@@ -16,19 +16,17 @@ function errorHandler(error) {
 }
 
 function clickHandler() {
-// outputDiv.innerText = "mjmjmjmjmj " + txtInput.value;
-  var inputText = txtInput.value; // taling input
+  var inputText = txtInput.value; // taking input
 
   // calling server for processing 
   fetch(getTranslationURL(inputText))
    .then(response => response.json())
    .then(json => {
      var translatedText = json.contents.translated;
-     outputDiv.innerText = translatedText;
+     outputDiv.innerText = translatedText; // output
     })
    .catch(errorHandler)
 
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
